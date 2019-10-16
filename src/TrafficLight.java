0fdf0f4710d0b1ab8fd90b5d2c254bcabeff42f4
zachary.sindelar <zachary.sindelar@my.jcu.edu.au>
2019-10-16 12:20:58 +1000

    basic traffic light operate method implemented

--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -2,5 +2,13 @@ public class TrafficLight {
     private double rateOfChange = 0.3;
     private String currentColor;
 
-    public void operate(){}
+    public void operate(){
+        double num = Math.random(); // gets random number between 0 and 1
+        double chanceOfChanging = Math.round(num); // rounds that number
+        if(chanceOfChanging <= rateOfChange) {
+            currentColor = "red";
+        }
+        else
+            currentColor = "green";
+    }
 }
