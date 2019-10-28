dd1b7d34e4786f6698c4216796ee34d2b634eb25
zachary.sindelar <zachary.sindelar@my.jcu.edu.au>
2019-10-28 11:05:10 +1000

    added a timer for the red traffic light

--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -1,12 +1,23 @@
 public class TrafficLight {
     private double rateOfChange = 0.3;
     private String currentColor = "green";
+    private int redTime = 0;
 
     public String getCurrentColor(){
         return currentColor;
     }
 
     public void operate(){
+
+        if (currentColor.equals("red")){ //forces the light to stay red for a set number of cycles
+            rateOfChange = 1;
+            redTime += 1;
+        }
+
+        if(redTime == 3){  // gets the light changing colors after a certain amount of time elapses
+            redTime = 0;
+            rateOfChange = 0.3;
+        }
         double num = Math.random(); // gets random number between 0 and 1
         if(num < rateOfChange) {  // checks whether light should be green or red
             currentColor = "red";
